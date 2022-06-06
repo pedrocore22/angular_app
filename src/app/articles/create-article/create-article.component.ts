f59b1449c4aa92e57460889b3531e79f93101b03
pedrocore22 <pedrocore22@itweblearning.com>
2022-06-06 14:54:23 +0200

    validation

--- src/app/articles/create-article/create-article.component.ts
+++ src/app/articles/create-article/create-article.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-create-article',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateArticleComponent implements OnInit {
 
+  form: FormGroup = new FormGroup({});
+
   constructor() { }
 
   ngOnInit(): void {
+    this.form = new FormGroup({
+      sku: new FormControl('', [Validators.required, Validators.minLength(4)]),
+      marca: new FormControl('', [Validators.required]),
+      modelo: new FormControl('', [Validators.required]),
+      genero: new FormControl(''),
+      descripcion: new FormControl('')
+    })
   }
 
 }
