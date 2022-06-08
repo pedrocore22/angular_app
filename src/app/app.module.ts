8d8ec99a0fc684401242d7dad3183e6d486e5408
pedrocore22 <pedrocore22@itweblearning.com>
2022-06-08 09:08:19 +0200

    spinner

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { SideNavComponent } from './side-nav/side-nav.component';
 import { HttpClientModule } from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
 import { CreateArticleComponent } from './articles/create-article/create-article.component';
+import { SpinnerComponent } from './spinner/spinner.component';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import { CreateArticleComponent } from './articles/create-article/create-article
     ArticlesDashboardComponent,
     HeaderNavComponent,
     SideNavComponent,
-    CreateArticleComponent
+    CreateArticleComponent,
+    SpinnerComponent
   ],
   imports: [
     BrowserModule,
