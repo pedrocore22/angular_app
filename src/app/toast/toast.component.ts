c6247d6c90df1eb207567f86ab9f7c848e85b308
pedrocore22 <pedrocore22@itweblearning.com>
2022-06-10 14:57:26 +0200

    timer

--- src/app/toast/toast.component.ts
+++ src/app/toast/toast.component.ts
@@ -15,20 +15,18 @@ export class ToastComponent implements OnInit {
   constructor(private toastService: ToastService) { }
 
   ngOnInit(): void {
+    let timer: any;
     this.toastService.getToastMessages()
-                     .subscribe((data: any) => {  
-                        let timer;
-                        if (this.isShowToast) {
-                          timer = setTimeout(() => {
-                            this.isShowToast = false;
-                          }, 0);
+                     .subscribe((data: any) => {
+                        if(this.isShowToast) {
+                          clearTimeout(timer);
                         } 
                         this.isShowToast = true;
+                        this.messageToast = data.messageToast;
+                        this.classToast = data.classToast;
                         timer = setTimeout(() => {
                           this.isShowToast = false;
                         }, 5000);        
-                        this.messageToast = data.messageToast;
-                        this.classToast = data.classToast;
 
                      })
   }
