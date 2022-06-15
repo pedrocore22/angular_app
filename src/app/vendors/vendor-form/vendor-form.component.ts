c653c781696269cc6541598597d668c35a66a094
pedrocore22 <pedrocore22@itweblearning.com>
2022-06-15 10:17:31 +0200

    patterns

--- src/app/vendors/vendor-form/vendor-form.component.ts
+++ src/app/vendors/vendor-form/vendor-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-vendor-form',
@@ -7,9 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VendorFormComponent implements OnInit {
 
+  form: FormGroup = new FormGroup({});
+
   constructor() { }
 
   ngOnInit(): void {
+    this.form = new FormGroup({
+      nombre: new FormControl('', [Validators.required]),
+      cif: new FormControl('', [Validators.required, 
+                                Validators.pattern(/([ABCDEFGHPQS])([0-9]{8})/i)]),
+      direccion: new FormControl(''),
+      localidad: new FormControl(''),
+      email: new FormControl('', [Validators.required,
+                                  Validators.pattern(/[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$/)])                          
+    })
   }
 
 }
