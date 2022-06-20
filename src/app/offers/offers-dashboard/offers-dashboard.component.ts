0e11e674d7afb32536c1c9e46c4e92c739b4c326
pedrocore22 <pedrocore22@itweblearning.com>
2022-06-20 12:42:13 +0200

    offers

--- src/app/offers/offers-dashboard/offers-dashboard.component.ts
+++ src/app/offers/offers-dashboard/offers-dashboard.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { ArticlesService } from 'src/app/services/articles.service';
+import { OffersService } from 'src/app/services/offers.service';
+import { VendorsService } from 'src/app/services/vendors.service';
 
 @Component({
   selector: 'app-offers-dashboard',
@@ -11,14 +13,20 @@ export class OffersDashboardComponent implements OnInit {
 
   form: FormGroup = new FormGroup({});
   articles: Array<any> = [];
+  vendors: Array<any> = [];
+  offers: Array<any> = [];
 
-  constructor(private articlesService: ArticlesService) { }
+  constructor(private articlesService: ArticlesService,
+              private vendorsService: VendorsService,
+              private offersService: OffersService) { }
 
   ngOnInit(): void {
     this.form = new FormGroup({
-      articuloTerm: new FormControl('')
+      articuloTerm: new FormControl(''),
+      proveedorTerm: new FormControl('')
     })
     this.searchArticles();
+    this.searchVendors();
   }
 
   searchArticles() {
@@ -39,4 +47,53 @@ export class OffersDashboardComponent implements OnInit {
                 }
               })
   }
+
+  searchVendors() {
+    this.form.get('proveedorTerm')?.valueChanges
+             .subscribe((data: any) => {
+                if(data.length > 0) {
+                  this.vendorsService.searchVendors(data)
+                                      .subscribe({
+                                        next: (data: any) => {
+                                          this.vendors = data;
+                                        },
+                                        error: (err: any) => {
+                                          console.log(err);
+                                        }
+                                      })
+                } else {
+                  this.vendors = [];
+                }
+              })
+  }
+
+
+  setSelectedArticle(article: any): void {
+    this.offersService.getOffersByArticle(article.id)
+                      .subscribe({
+                        next: (data: any) => {
+                          this.articles = [];
+                          this.form.get('articuloTerm')?.setValue('');
+                          this.offers = data;
+                        },
+                        error: (err: any) => {
+                          console.log(err);
+                        }
+                      })
+  }
+
+  setSelectedVendor(vendor: any): void {
+    this.offersService.getOffersByVendor(vendor.id)
+                      .subscribe({
+                        next: (data: any) => {
+                          this.vendors = [];
+                          this.form.get('proveedorTerm')?.setValue('');
+                          this.offers = data;
+                        },
+                        error: (err: any) => {
+                          console.log(err);
+                        }
+                      })
+  }
+
 }
